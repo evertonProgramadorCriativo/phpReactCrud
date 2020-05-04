3d2ef5b37240d504ae9f1727b0133bcb6bee03d8
evertonProgramadorCriativo <evertonprogramadorcriativo@gmail.com>
2020-05-04 09:20:17 -0300

    error inserit

--- src/components/Insert.js
+++ src/components/Insert.js
@@ -1,25 +1,81 @@
 import React, { Component } from "react";
 
 export default class Insert extends Component {
+ constructor(props) {
+   super(props);
+   this.onChangeFirstName = this.onChangeFirstName.bind(this);
+   this.onChangeLastName = this.onChangeLastName.bind(this);
+   this.onChangeEmail = this.onChangeEmail.bind(this);
+   this.onSubmit = this.onSubmit.bind(this);
+ 
+         this.state = {
+           first_name: '',
+           last_name: '',
+           email:''
+         }
+ 
+  }
+
+  onChangeFirstName(e) {
+    this.setState({
+      first_name: e.target.value
+    });
+  }
+
+  onChangeLastName(e) {
+    this.setState({
+      last_name: e.target.value
+    })
+  };
+
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const obj = {
+      first_name: this.state.first_name,
+      last_name: this.state.last_name,
+      email: this.state.email
+    };
+
+    console.log(obj);
+  }
+
+
+
   render() {
     return (
       <div style={{ marginTop: 10 }}>
         <h3>Add New User</h3>
 
-        <form>
+        <form onSubmit={this.onSubmit}>
           <div className="form-group">
             <label>First Name: </label>
-            <input type="text" className="form-control" />
+            <input type="text" className="form-control" 
+              value={this.state.first_name}
+              onChange={this.onChangeFirstName}
+            />
           </div>
 
           <div className="form-group">
             <label>Last Name: </label>
-            <input type="text" className="form-control" />
+            <input type="text" className="form-control" 
+                value={this.state.last_name}
+              onChange={this.onChangeLastName}
+            />
           </div>
 
           <div className="form-group">
             <label>Email: </label>
-            <input type="text" className="form-control" />
+            <input type="text" className="form-control"
+            value={this.state.email}
+              onChange={this.onChangeEmail}
+             />
           </div>
 
           <div className="form-group">
